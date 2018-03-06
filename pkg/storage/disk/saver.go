ef5dada0397e962f2bb1c5742bfaa95a8667424a
Aaron Schlesinger <arschles@gmail.com>
2018-03-06 15:51:18 -0800

    Shorter var name

--- pkg/storage/disk/saver.go
+++ pkg/storage/disk/saver.go
@@ -7,19 +7,20 @@ import (
 )
 
 func (s *storageImpl) Save(baseURL, module, vsn string, mod, zip []byte) error {
-	containingDir := s.versionDiskLocation(baseURL, module, vsn)
+	dir := s.versionDiskLocation(baseURL, module, vsn)
 	// TODO: 777 is not the best filemode, use something better
 
 	// make the versioned directory to hold the go.mod and the zipfile
-	if err := os.MkdirAll(containingDir, 777); err != nil {
+	if err := os.MkdirAll(dir, 777); err != nil {
+		fmt.Printf("error creating dir %s (%s)\n", dir, err)
 		return err
 	}
 
 	// write the go.mod file
-	if err := ioutil.WriteFile(filepath.Join(containingDir, "go.mod"), mod, 777); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(dir, "go.mod"), mod, 777); err != nil {
 		return err
 	}
 
 	// write the zipfile
-	return ioutil.WriteFile(filepath.Join(containingDir, "source.zip"), zip, 777)
+	return ioutil.WriteFile(filepath.Join(dir, "source.zip"), zip, 777)
 }
