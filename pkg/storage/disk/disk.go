81d898ff95c8691d6a8fa20415039666d6f73df1
Aaron Schlesinger <arschles@gmail.com>
2018-03-06 15:20:28 -0800

    implementing disk-based version listing

--- pkg/storage/disk/disk.go
+++ pkg/storage/disk/disk.go
@@ -19,6 +19,15 @@ type storageImpl struct {
 	rootDir string
 }
 
+func (s *storageImpl) moduleDiskLocation(baseURL, module string) string {
+	return filepath.Join(s.rootDir, baseURL, module)
+}
+
+func (s *storageImpl) versionDiskLocation(baseURL, module, version string) string {
+	return filepath.Join(s.moduleDiskLocation(baseURL, module), version)
+
+}
+
 // NewStorage returns a new ListerSaver implementation that stores
 // everything under rootDir
 func NewStorage(rootDir string) Storage {
--- pkg/storage/disk/lister.go
+++ pkg/storage/disk/lister.go
@@ -1,13 +1,20 @@
 package disk
 
 import (
-	"github.com/gomods/athens/pkg/storage"
+	"io/ioutil"
 )
 
 func (l *storageImpl) List(basePath, module string) ([]string, error) {
-	return nil, nil
-}
-
-func (l *storageImpl) All() (map[string][]*storage.RevInfo, error) {
-	return nil, nil
+	loc := l.moduleDiskLocation(basePath, module)
+	fileInfos, err := ioutil.ReadDir(loc)
+	if err != nil {
+		return nil, err
+	}
+	ret := []string{}
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			ret = append(ret, fileInfo.Name())
+		}
+	}
+	return ret, nil
 }
