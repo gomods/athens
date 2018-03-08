5e199ed19a0e2b34ee9c12fccec17d52047245ab
Aaron Schlesinger <arschles@gmail.com>
2018-03-08 16:22:45 -0500

    Partially implementing the getter

--- pkg/storage/disk/getter.go
+++ pkg/storage/disk/getter.go
@@ -1,9 +1,19 @@
 package disk
 
 import (
+	"time"
+
 	"github.com/gomods/athens/pkg/storage"
 )
 
 func (v *storageImpl) Get(baseURL, module, vsn string) (*storage.Version, error) {
-	return nil, nil
+	// TODO: store the time in the saver, and parse it here
+	return &storage.Version{
+		RevInfo: storage.RevInfo{
+			Version: vsn,
+			Name:    vsn,
+			Short:   vsn,
+			Time:    time.Now(),
+		},
+	}, nil
 }
