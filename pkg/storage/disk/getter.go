732d30b118d9b909dd39049959834a9711a9fe2e
Aaron Schlesinger <arschles@gmail.com>
2018-03-08 16:27:11 -0500

    Finishing implementation of the get function

--- pkg/storage/disk/getter.go
+++ pkg/storage/disk/getter.go
@@ -1,12 +1,26 @@
 package disk
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"time"
 
 	"github.com/gomods/athens/pkg/storage"
 )
 
 func (v *storageImpl) Get(baseURL, module, vsn string) (*storage.Version, error) {
+	versionedPath := v.versionDiskLocation(baseURL, module, vsn)
+
+	mod, err := ioutil.ReadFile(filepath.Join(versionedPath, "go.mod"))
+	if err != nil {
+		return nil, err
+	}
+
+	src, err := ioutil.ReadFile(filepath.Join(versionedPath, "source.zip"))
+	if err != nil {
+		return nil, err
+	}
+
 	// TODO: store the time in the saver, and parse it here
 	return &storage.Version{
 		RevInfo: storage.RevInfo{
@@ -15,5 +29,7 @@ func (v *storageImpl) Get(baseURL, module, vsn string) (*storage.Version, error)
 			Short:   vsn,
 			Time:    time.Now(),
 		},
+		Mod: mod,
+		Zip: src,
 	}, nil
 }
