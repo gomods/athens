4e88e3598c65b2ee322d7b243289f429f371d280
Brian Ketelsen <bketelsen@gmail.com>
2018-03-20 17:02:47 -0400

    get rid of debug

--- actions/home.go
+++ actions/home.go
@@ -5,12 +5,9 @@ import (
 )
 
 func proxyHomeHandler(c buffalo.Context) error {
-	c.Flash().Add("info", "Proxy")
 	return c.Render(200, proxy.HTML("index.html"))
 }
 
 func homeHandler(c buffalo.Context) error {
-	c.Flash().Add("info", "Registry")
-
 	return c.Render(200, registry.HTML("index.html"))
 }
