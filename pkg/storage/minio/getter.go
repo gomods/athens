6b6c6539527aad61a96ac99877c6fb24a990acfc
robjloranger <robjloranger@protonmail.com>
2018-05-25 16:54:31 -0700

    Add .info to upload payload everywhere else as well
    + Upload payload includes Info
    + CLI uploads `version.info` as blob to storage
    + Getter and Saver methods updated for all storages implemented
    + Migration updated to include Info column
    + Tests no longer check RevInfo, now checking for Info same as Zip
    + Updated docs

--- pkg/storage/minio/getter.go
+++ pkg/storage/minio/getter.go
@@ -3,7 +3,6 @@ package minio
 import (
 	"fmt"
 	"io/ioutil"
-	"time"
 
 	"github.com/gomods/athens/pkg/storage"
 	minio "github.com/minio/minio-go"
@@ -22,16 +21,16 @@ func (v *storageImpl) Get(module, version string) (*storage.Version, error) {
 	if err != nil {
 		return nil, err
 	}
+	infoPath := fmt.Sprintf("%s/%s.info", versionedPath, version)
+	infoReader, err := v.minioClient.GetObject(v.bucketName, infoPath, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
 
 	// TODO: store the time in the saver, and parse it here
 	return &storage.Version{
-		RevInfo: storage.RevInfo{
-			Version: version,
-			Name:    version,
-			Short:   version,
-			Time:    time.Now(),
-		},
-		Mod: mod,
-		Zip: zipReader,
+		Mod:  mod,
+		Zip:  zipReader,
+		Info: infoReader,
 	}, nil
 }
