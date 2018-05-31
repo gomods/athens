02e85911dad0e7f2e546e1e2a72bc2d99392727d
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 14:22:49 +0000

    Adding a reference to a deleter in the merger

--- pkg/eventlog/merger.go
+++ pkg/eventlog/merger.go
@@ -1,5 +1,9 @@
 package eventlog
 
+import (
+	"github.com/gomods/athens/pkg/storage"
+)
+
 // Merger merges a single eventlog entry into the event log database. Merging
 // means:
 //
@@ -7,9 +11,12 @@ package eventlog
 // - Modifying the module database accordingly
 type Merger struct {
 	getter   storage.Getter
+	deleter  storage.Deleter
+	setter   storage.Saver
 	appender Appender
 }
 
 func (m Merger) Merge(evt Event) error {
 	// TODO
+	return nil
 }
