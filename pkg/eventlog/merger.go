0228c9dac0a72e01067ec89662364d07119dd97f
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 15:21:31 +0000

    Making the merger use CDN actions only

--- pkg/eventlog/merger.go
+++ pkg/eventlog/merger.go
@@ -1,7 +1,7 @@
 package eventlog
 
 import (
-	"github.com/gomods/athens/pkg/storage"
+	"github.com/gomods/athens/pkg/cdn"
 )
 
 // Merger merges a single eventlog entry into the event log database. Merging
@@ -10,24 +10,22 @@ import (
 // - Appending the log entry into the event log
 // - Modifying the module database accordingly
 type Merger struct {
-	getter   storage.Getter
-	deleter  storage.Deleter
-	setter   storage.Saver
-	appender Appender
+	getter    cdn.Getter
+	deleter   cdn.Deleter
+	mdSaver   cdn.MetadataSaver
+	dataSaver cdn.DataSaver
+	appender  Appender
 }
 
 // Merge will merge evt into the event log, ensuring that globally, all
 // operations on the event log are serialized. After merging, it will take
 // appropriate action on the module in storage. For example, if the event action
-// is to add a new module, Merge will append that action to the log and add
-// the module to storage.
+// is to add a new module, Merge will do the following:
 //
-// If the receiving Olympus (OX) gets an add event from OY, OX will add the
-// module in a two step process:
-//
-// - Store the CDN location as OY's CDN
-// - Download module metadata and source from OY's CDN
-// - Update the CDN location to OX's CDN
+// - Append that action to the log
+// - Download module metadata and source code from the other Olympus
+// - Store the module metadata & source code in its CDN
+// - Add the existence of the module metadata & source code in its module metadata storage
 func (m Merger) Merge(evt Event) error {
 	// TODO
 	return nil
