8feaffe5904e09324281f9f1fddbe593c10e4bac
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 15:09:19 +0000

    Adding an ErrNotFound error to the CDN package

--- pkg/cdn/cdn.go
+++ pkg/cdn/cdn.go
@@ -1,5 +1,21 @@
 package cdn
 
+import (
+	"fmt"
+)
+
+// ErrNotFound ios an error implementation to indicate that a module is not
+// found
+type ErrNotFound struct {
+	ModuleName    string
+	ModuleVersion string
+}
+
+// Error is the error interface implementation
+func (e ErrNotFound) Error() string {
+	return fmt.Sprintf("Module %s/%s not found", e.ModuleName, e.ModuleVersion)
+}
+
 // CDN represents access to a CDN for registry use
 type CDN interface {
 	Saver
