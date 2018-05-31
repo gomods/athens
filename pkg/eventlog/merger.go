f9aea4e8d39fc486695b33d7c4858cd08ca5967e
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 15:01:52 +0000

    adding docs to the event log merger

--- pkg/eventlog/merger.go
+++ pkg/eventlog/merger.go
@@ -16,6 +16,18 @@ type Merger struct {
 	appender Appender
 }
 
+// Merge will merge evt into the event log, ensuring that globally, all
+// operations on the event log are serialized. After merging, it will take
+// appropriate action on the module in storage. For example, if the event action
+// is to add a new module, Merge will append that action to the log and add
+// the module to storage.
+//
+// If the receiving Olympus (OX) gets an add event from OY, OX will add the
+// module in a two step process:
+//
+// - Store the CDN location as OY's CDN
+// - Download module metadata and source from OY's CDN
+// - Update the CDN location to OX's CDN
 func (m Merger) Merge(evt Event) error {
 	// TODO
 	return nil
