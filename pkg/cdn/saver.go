cdcb72e63c1b673b4b2aaee68386d51382d7e61a
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 15:09:35 +0000

    Adding more docs

--- pkg/cdn/saver.go
+++ pkg/cdn/saver.go
@@ -6,5 +6,7 @@ import (
 
 // Saver saves a module metadata & storage to its underlying storage
 type Saver interface {
+	// Save saves the given module and metadata and source code to
+	// the CDN and records its location in module metadata storage
 	Save(module string, version *storage.Version) error
 }
