d5a4b85492872d1beba8a942095a6a5460706ab8
Aaron Schlesinger <arschles@gmail.com>
2018-05-31 16:59:42 +0000

    remove out-of-scope changes
    They'll be added as appropriate in follow-ups

--- pkg/cdn/deleter.go
+++
@@ -1,9 +0,0 @@
-package cdn
-
-// Deleter deletes a module from the CDN and its metadata from metadata storage
-type Deleter interface {
-	// Delete removes module/version from the CDN and its metadata storage.
-	// Returns ErrNotFound if the module/version isn't found, and another
-	// non-nil error on any other error encountered
-	Delete(module string, version string) error
-}
--- pkg/cdn/metadata_saver.go
+++
@@ -1,8 +0,0 @@
-package cdn
-
-// MetadataSaver saves metadata about the module/version's location in a CDN.
-// Returns a non-nil error if there were any issues saving this information
-type MetadataSaver interface {
-	// Save saves the module/version information to metadata storage
-	Save(moduleName, version, cdnLocation string) error
-}
