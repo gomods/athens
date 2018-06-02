1fe74dac8ddf90c465e59a5da49c3eca99f1bbe4
Aaron Schlesinger <arschles@gmail.com>
2018-06-02 11:57:34 +0000

    Adding CDN metadata functions

--- pkg/cdn/saver.go
+++ pkg/cdn/saver.go
@@ -1,10 +1,14 @@
 package cdn
 
 import (
+	"github.com/gomods/athens/pkg/eventlog"
 	"github.com/gomods/athens/pkg/storage"
 )
 
 // Saver saves a module metadata & storage to its underlying storage
 type Saver interface {
+	// Save saves the given module and version to the CDN
 	Save(module string, version *storage.Version) error
+	// SaveMetadata saves "op" to the module key/value store for module/version
+	SaveMetadata(module, version string, op eventlog.Op) error
 }
