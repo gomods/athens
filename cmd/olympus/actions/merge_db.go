3703eaf01f8c76389c66e05523353657a214d257
Aaron Schlesinger <arschles@gmail.com>
2018-06-06 12:13:23 -0700

    making loops continue instead of bailing out
    also adding logs on errors

--- cmd/olympus/actions/merge_db.go
+++ cmd/olympus/actions/merge_db.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"log"
+
 	"github.com/gomods/athens/pkg/cdn"
 	"github.com/gomods/athens/pkg/eventlog"
 )
@@ -21,50 +23,58 @@ func mergeDB(originURL string, diff dbDiff, getter cdn.Getter, saver cdn.Saver, 
 		if _, err := getter.GetForVersion(added.Module, added.Version); err != nil {
 			// the module/version already exists, is deprecated, or is
 			// tombstoned, so nothing to do
-			break
+			continue
 		}
 
 		// download code from the origin
 		data, err := cdn.Download(originURL, added.Module, added.Version)
 		if err != nil {
-			return err
+			log.Printf("error downloading new module %s/%s from %s (%s)", added.Module, added.Version, originURL, err)
+			continue
 		}
 
 		// save module data to the CDN
 		if err := saver.Save(added.Module, data); err != nil {
-			return err
+			log.Printf("error saving new module %s/%s to CDN (%s)", added.Module, added.Version, err)
+			continue
 		}
 
 		// save module metadata to the key/value store
 		if err := saver.SaveMetadata(added.Module, added.Version, eventlog.OpAdd); err != nil {
-			return err
+			log.Printf("error saving metadata for new module %s/%s (%s)", added.Module, added.Version, err)
+			continue
 		}
 	}
 	for _, deprecated := range diff.Deprecated {
 		fromDB, err := getter.GetForVersion(deprecated.Module, deprecated.Version)
 		if err != nil {
-			return err
+			log.Printf("error getting deprecated module %s/%s (%s)", deprecated.Module, deprecated.Version, err)
+			continue
 		}
 		if fromDB.Op == eventlog.OpDel {
-			break // can't deprecate something that's already deleted
+			continue // can't deprecate something that's already deleted
 		}
 		// delete from the CDN
 		if err := deleter.Delete(deprecated.Module, deprecated.Version); err != nil {
-			return err
+			log.Printf("error deleting deprecated module %s/%s from CDN (%s)", deprecated.Module, deprecated.Version, err)
+			continue
 		}
 
 		// add the tombstone to module metadata
 		if err := saver.SaveMetadata(deprecated.Module, deprecated.Version, eventlog.OpDep); err != nil {
-			return err
+			log.Printf("error saving metadata for deprecated module %s/%s from CDN (%s)", deprecated.Module, deprecated.Version, err)
+			continue
 		}
 	}
 	for _, deleted := range diff.Deleted {
 		// delete in the CDN
 		if err := deleter.Delete(deleted.Module, deleted.Version); err != nil {
-			return err
+			log.Printf("error deleting deleted module %s/%s from CDN (%s)", deleted.Module, deleted.Version, err)
+			continue
 		}
 		// add tombstone to module metadata
 		if err := saver.SaveMetadata(deleted.Module, deleted.Version, eventlog.OpDel); err != nil {
+			log.Printf("error inserting tombstone for deleted module %s/%s (%s)", deleted.Module, deleted.Version, err)
 			return err
 		}
 	}
