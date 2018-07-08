8259400dfaa0dc07019c647d1f87a23761caf3e8
marwan-at-work <marwan.sameer@gmail.com>
2018-07-08 19:05:58 -0400

    Olympus: add PATH to vgo get

--- pkg/repo/goget.go
+++ pkg/repo/goget.go
@@ -123,7 +123,7 @@ func getSources(fs afero.Fs, gopath, repoRoot, repoURI, version string) (string,
 	disableCgo := "CGO_ENABLED=0"
 
 	cmd := exec.Command("vgo", "get", fullURI)
-	cmd.Env = []string{gopathEnv, disableCgo}
+	cmd.Env = []string{os.Getenv("PATH"), gopathEnv, disableCgo} // breaks windows.
 	cmd.Dir = repoRoot
 
 	packagePath := filepath.Join(gopath, "src", "mod", "cache", "download", repoURI, "@v")
