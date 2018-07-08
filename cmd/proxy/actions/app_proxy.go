ff7a099280609424d4653ad5e15e21805bbbf51e
marwan-at-work <marwan.sameer@gmail.com>
2018-07-08 14:51:01 -0400

    Zeus: temp fix for upload regex + handler fixes

--- cmd/proxy/actions/app_proxy.go
+++ cmd/proxy/actions/app_proxy.go
@@ -11,7 +11,7 @@ func addProxyRoutes(app *buffalo.App, storage storage.Backend) error {
 	app.GET("/{module:.+}/@v/{version}.info", cacheMissHandler(versionInfoHandler(storage), app.Worker))
 	app.GET("/{module:.+}/@v/{version}.mod", cacheMissHandler(versionModuleHandler(storage), app.Worker))
 	app.GET("/{module:.+}/@v/{version}.zip", cacheMissHandler(versionZipHandler(storage), app.Worker))
-	app.POST("/admin/upload/{module:[a-zA-Z./]+}/{version}", uploadHandler(storage))
-	app.POST("/admin/fetch/{module:[a-zA-Z./]+}/{owner}/{repo}/{ref}/{version}", fetchHandler(storage))
+	app.POST("/admin/upload/{module:.+}/{version}", uploadHandler(storage))
+	app.POST("/admin/fetch/{module:.+}/{owner}/{repo}/{ref}/{version}", fetchHandler(storage))
 	return nil
 }
