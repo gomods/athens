9ca369b099d1cc4ee7f29662d644174941ca7eff
Aaron Schlesinger <arschles@gmail.com>
2018-07-09 14:13:37 -0700

    moving the buffalo binary to a permitted place
    also making the binary executable

--- scripts/get_buffalo.sh
+++ scripts/get_buffalo.sh
@@ -2,7 +2,9 @@
 
 BUFFALO_URL="https://github.com/gobuffalo/buffalo/releases/download/v0.12.3/buffalo_0.12.3_linux_amd64.tar.gz"
 TAR_GZ="buffalo_0.12.3_linux_amd64.tar.gz"
+BUFFALO_TARGET_BIN="./bin/buffalo"
 
 curl -L -o ${TAR_GZ} ${BUFFALO_URL}
 tar -xvf ${TAR_GZ}
-mv buffalo-no-sqlite /usr/local/bin/buffalo
+mv buffalo-no-sqlite ${BUFFALO_TARGET_BIN}
+chmod +x ${BUFFALO_TARGET_BIN}
