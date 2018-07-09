2bf2c5ba042f967701cdef5c3f1b37f9b1d6faaf
Aaron Schlesinger <arschles@gmail.com>
2018-07-09 14:17:53 -0700

    composing buffalo URL with tar_gz env var

--- scripts/get_buffalo.sh
+++ scripts/get_buffalo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BUFFALO_URL="https://github.com/gobuffalo/buffalo/releases/download/v0.12.3/buffalo_0.12.3_linux_amd64.tar.gz"
 TAR_GZ="buffalo_0.12.3_linux_amd64.tar.gz"
+BUFFALO_URL="https://github.com/gobuffalo/buffalo/releases/download/v0.12.3/${TAR_GZ}"
 BUFFALO_TARGET_BIN="./bin/buffalo"
 
 curl -L -o ${TAR_GZ} ${BUFFALO_URL}
