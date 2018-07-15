031d2b9f8df0b0f4937a5570ff9f94817feafbd4
marwan-at-work <marwan.sameer@gmail.com>
2018-07-14 23:02:54 -0400

    log: add buffalo doc note

--- pkg/log/buffalo.go
+++ pkg/log/buffalo.go
@@ -12,6 +12,9 @@ import (
 // than the default buffalo formatter.
 // For the most part, we only care about
 // the path, the method, and the status code.
+// It's also good to note that internal logs
+// from buffalo should only be allowed in development
+// as our logging-system should be handled from our codebase.
 func Buffalo() buffalo.Logger {
 	l := logrus.New()
 	l.Formatter = &buffaloFormatter{}
