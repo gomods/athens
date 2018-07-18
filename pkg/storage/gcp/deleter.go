e88d919058411d5ebbbb65879ce1bf1371ce5fe8
marpio <marcinkowski@mailbox.org>
2018-07-19 00:03:21 +0200

    use delete in gcp

--- pkg/storage/gcp/deleter.go
+++ pkg/storage/gcp/deleter.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gomods/athens/pkg/config"
 	"github.com/gomods/athens/pkg/storage"
-	multierror "github.com/hashicorp/go-multierror"
+	modupl "github.com/gomods/athens/pkg/storage/module"
 )
 
 // Delete implements the (./pkg/storage).Deleter interface and
@@ -16,15 +16,6 @@ func (s *Storage) Delete(module, version string) error {
 	if exists := s.bucket.Exists(ctx, config.PackageVersionedName(module, version, "mod")); !exists {
 		return storage.ErrVersionNotFound{Module: module, Version: version}
 	}
-	var errs error
-	if err := s.bucket.Delete(ctx, config.PackageVersionedName(module, version, "mod")); err != nil {
-		errs = multierror.Append(errs, err)
-	}
-	if err := s.bucket.Delete(ctx, config.PackageVersionedName(module, version, "info")); err != nil {
-		errs = multierror.Append(errs, err)
-	}
-	if err := s.bucket.Delete(ctx, config.PackageVersionedName(module, version, "zip")); err != nil {
-		errs = multierror.Append(errs, err)
-	}
-	return errs
+
+	return modupl.Delete(ctx, module, version, s.bucket.Delete)
 }
