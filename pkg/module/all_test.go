bf325dbe1f4c70d9f084e930d0c003b1dbc896f4
Aaron Schlesinger <arschles@gmail.com>
2018-07-18 18:24:26 -0700

    adding a test for the disk ref

---
+++ pkg/module/all_test.go
@@ -0,0 +1,21 @@
+package module
+
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/suite"
+)
+
+type ModuleSuite struct {
+	suite.Suite
+	fs afero.Fs
+}
+
+func (m *ModuleSuite) BeforeTest() {
+	m.fs = afero.NewMemMapFs()
+}
+
+func TestModules(t *testing.T) {
+	suite.Run(t, &ModuleSuite{})
+}
