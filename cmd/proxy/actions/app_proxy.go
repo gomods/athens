e94cc9625b1219bed6d7989d97bc7a7f4c75744c
Aaron Schlesinger <arschles@gmail.com>
2018-07-20 13:27:10 -0700

    Remove the fetch endpoint from the proxy
    This removes code that is no longer used and depends on the ./pkg/repo package

--- cmd/proxy/actions/app_proxy.go
+++ cmd/proxy/actions/app_proxy.go
@@ -21,7 +21,5 @@ func addProxyRoutes(
 	app.GET(download.PathVersionInfo, cacheMissHandler(download.VersionInfoHandler(storage, proxy), app.Worker, mf, lggr))
 	app.GET(download.PathVersionModule, cacheMissHandler(download.VersionModuleHandler(storage, proxy), app.Worker, mf, lggr))
 	app.GET(download.PathVersionZip, cacheMissHandler(download.VersionZipHandler(storage), app.Worker, mf, lggr))
-
-	app.POST("/admin/fetch/{module:[a-zA-Z./]+}/{owner}/{repo}/{ref}/{version}", fetchHandler(storage))
 	return nil
 }
