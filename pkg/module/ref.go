9d1cfc61f5131db03f5f8228416f0e69967ce958
Aaron Schlesinger <arschles@gmail.com>
2018-07-25 14:16:15 -0700

    adding comment about closing the ver.Zip

--- pkg/module/ref.go
+++ pkg/module/ref.go
@@ -9,6 +9,8 @@ type Ref interface {
 	// Clear frees the storage & resources that the module uses. Calls to Read after you call
 	// this function may fail, regardless of whether this function returns nil or not
 	Clear() error
-	// Read reads the module into memory and returns it
+	// Read reads the module into memory and returns it. Notice that the Zip field on the returned
+	// storage.Version is an io.ReadCloser, so make sure to call Close on it after you're done
+	// with it. ref.Clear won't do it for you!
 	Read() (*storage.Version, error)
 }
