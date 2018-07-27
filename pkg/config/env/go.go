5b9394ef6c1a24898d9f01f4f8df3fed41f10d5f
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-07-27 14:45:44 -0500

    Carolyn tries to fix Aaron's code

--- pkg/config/env/go.go
+++ pkg/config/env/go.go
@@ -22,15 +22,8 @@ func GoPath() (string, error) {
 	return env, nil
 }
 
-<<<<<<< HEAD
-// GoBinPath returns the path to Go's executable binary
-// this binary must have Go Modules enabled.
-func GoBinPath() string {
-	return envy.Get("GO_BIN_PATH", "vgo")
-=======
 // GoBinaryPath returns the path to the go binary to use, defined by
 // GO_BINARY_PATH. This value can be a name on the PATH, or the full path
 func GoBinaryPath() string {
 	return envy.Get("GO_BINARY_NAME", "go")
->>>>>>> add middlewares to fill the cache and to populate contexts with module and versions
 }
--- pkg/download/version_module.go
+++ pkg/download/version_module.go
@@ -6,19 +6,30 @@ import (
 	"github.com/bketelsen/buffet"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/render"
-	"github.com/gomods/athens/pkg/storage"
+	"github.com/gomods/athens/pkg/log"
+	"github.com/gomods/athens/pkg/errors"
 )
 
 // PathVersionModule URL.
 const PathVersionModule = "/{module:.+}/@v/{version}.mod"
 
 // VersionModuleHandler implements GET baseURL/module/@v/version.mod
-func VersionModuleHandler(dp download.Protocol, lggr *log.Logger, eng *render.Engine) buffalo.Handler {
-	return func(c buffalo.Context, module, version string, versionInfo *storage.Version) error {
+func VersionModuleHandler(dp Protocol, lggr *log.Logger, eng *render.Engine) buffalo.Handler {
+	return func(c buffalo.Context) error {
+		const op errors.Op = "download.VersionModuleHandler"
+
 		sp := buffet.SpanFromContext(c)
 		sp.SetOperationName("versionModuleHandler")
+
+		mod, ver, verInfo, err := getModuleVersion(c, lggr, dp)
+		if err != nil {
+			err := errors.E(op, errors.M(mod), errors.V(ver), err)
+			lggr.SystemErr(err)
+			c.Render(http.StatusInternalServerError, nil)
+		}
+
 		c.Response().WriteHeader(http.StatusOK)
-		_, err := c.Response().Write(versionInfo.Mod)
+		_, err = c.Response().Write(verInfo.Mod)
 		return err
 	}
 }
