4379bcd17c646701fcac3b0d92760fbd6deae3a4
marwan-at-work <marwan.sameer@gmail.com>
2018-07-27 14:17:23 -0400

    download: document Protocol

--- pkg/download/download.go
+++ pkg/download/download.go
@@ -11,11 +11,22 @@ import (
 // Protocol is the download protocol which mirrors
 // the http requests that cmd/go makes to the proxy.
 type Protocol interface {
+	// List implements GET /{module}/@v/list
 	List(ctx context.Context, mod string) ([]string, error)
+
+	// Info implements GET /{module}/@v/{version}.info
 	Info(ctx context.Context, mod, ver string) ([]byte, error)
+
+	// Latest implements GET /{module}/@latest
 	Latest(ctx context.Context, mod string) (*storage.RevInfo, error)
+
+	// GoMod implements GET /{module}/@v/{version}.mod
 	GoMod(ctx context.Context, mod, ver string) ([]byte, error)
+
+	// Zip implements GET /{module}/@v/{version}.zip
 	Zip(ctx context.Context, mod, ver string) (io.ReadCloser, error)
+
+	// Version is a helper method to get Info, GoMod, and Zip together.
 	Version(ctx context.Context, mod, ver string) (*storage.Version, error)
 }
 
