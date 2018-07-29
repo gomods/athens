ef0f2f61715af6467a65bdb28d62e9daa169d3b1
Rohan Chakravarthy <rohanc219@gmail.com>
2018-07-28 18:42:13 -0700

    Ensure diskRef cleans up everything in it's own GOPATH

--- pkg/module/disk_ref.go
+++ pkg/module/disk_ref.go
@@ -3,6 +3,7 @@ package module
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	"github.com/gomods/athens/pkg/storage"
@@ -15,14 +16,16 @@ import (
 // Do not create this struct directly. use newDiskRef
 type diskRef struct {
 	root    string
+	module  string
 	fs      afero.Fs
 	version string
 }
 
-func newDiskRef(fs afero.Fs, root, version string) *diskRef {
+func newDiskRef(fs afero.Fs, root, module, version string) *diskRef {
 	return &diskRef{
 		fs:      fs,
 		root:    root,
+		module:  module,
 		version: version,
 	}
 }
@@ -31,6 +34,20 @@ func newDiskRef(fs afero.Fs, root, version string) *diskRef {
 //
 // You should always call this function after you fetch a module into a DiskRef
 func (d *diskRef) Clear() error {
+
+	// This is required because vgo ensures dependencies are read-only
+	// See https://github.com/golang/go/issues/24111 and
+	// https://go-review.googlesource.com/c/vgo/+/96978
+	walkFn := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		return d.fs.Chmod(path, 0770)
+	}
+	err := afero.Walk(d.fs, d.root, walkFn)
+	if err != nil {
+		return err
+	}
 	return d.fs.RemoveAll(d.root)
 }
 
@@ -38,7 +55,8 @@ func (d *diskRef) Clear() error {
 func (d *diskRef) Read() (*storage.Version, error) {
 	var ver storage.Version
 
-	infoFile, err := d.fs.Open(filepath.Join(d.root, fmt.Sprintf("%s.info", d.version)))
+	packagePath := getPackagePath(d.root, d.module)
+	infoFile, err := d.fs.Open(filepath.Join(packagePath, fmt.Sprintf("%s.info", d.version)))
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -50,7 +68,7 @@ func (d *diskRef) Read() (*storage.Version, error) {
 	}
 	ver.Info = info
 
-	modFile, err := d.fs.Open(filepath.Join(d.root, fmt.Sprintf("%s.mod", d.version)))
+	modFile, err := d.fs.Open(filepath.Join(packagePath, fmt.Sprintf("%s.mod", d.version)))
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
@@ -61,7 +79,7 @@ func (d *diskRef) Read() (*storage.Version, error) {
 	}
 	ver.Mod = mod
 
-	sourceFile, err := d.fs.Open(filepath.Join(d.root, fmt.Sprintf("%s.zip", d.version)))
+	sourceFile, err := d.fs.Open(filepath.Join(packagePath, fmt.Sprintf("%s.zip", d.version)))
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
