6ba5b94597bc144b81cc915621a27d6300afac90
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-07-30 11:29:25 -0500

    Make the go 1.11 source directory configurable
    Don't error out if it's been cloned already

--- scripts/get_dev_tools.sh
+++ scripts/get_dev_tools.sh
@@ -8,9 +8,11 @@ go get github.com/golang/lint/golint
 go get github.com/golang/dep/cmd/dep
 
 GO_VERSION="go1.11beta2"
-GO_SOURCE=$(go env GOPATH)/src/golang.org/x/go
+GO_SOURCE=${GO_SOURCE:=$(go env GOPATH)/src/golang.org/x/go}
 mkdir -p $(dirname $GO_SOURCE)
-git clone https://go.googlesource.com/go $GO_SOURCE
+if [[ ! -d $GO_SOURCE ]]; then
+  git clone https://go.googlesource.com/go $GO_SOURCE
+fi
 pushd $GO_SOURCE
 git checkout $GO_VERSION
 cd src && ./make.bash
