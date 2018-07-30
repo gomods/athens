2eedb80ab2ca010b75a61a5c68713b9c976e6ceb
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-07-30 10:21:58 -0500

    Fix comment

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# test_all.sh
-# Run all the tests with the race detector and code coverage enabled
+# test_unit.sh
+# Run the unit tests with the race detector and code coverage enabled
 set -xeuo pipefail
 
 go test -race -coverprofile cover.out -covermode atomic ./...
