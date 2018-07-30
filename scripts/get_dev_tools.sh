32fe18b45127ee33e5d41d93b12c3a5bfff0e157
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-07-30 10:19:53 -0500

    Use go1.11beta2 instead of vgo

--- scripts/get_dev_tools.sh
+++ scripts/get_dev_tools.sh
@@ -6,5 +6,14 @@ set -xeuo pipefail
 
 go get github.com/golang/lint/golint
 go get github.com/golang/dep/cmd/dep
-go get -u -v golang.org/x/vgo
+
+GO_VERSION="go1.11beta2"
+GO_SOURCE=$(go env GOPATH)/src/golang.org/x/go
+mkdir -p $(dirname $GO_SOURCE)
+git clone https://go.googlesource.com/go $GO_SOURCE
+pushd $GO_SOURCE
+git checkout $GO_VERSION
+cd src && ./make.bash
+popd
+
 ./scripts/get_buffalo.sh
