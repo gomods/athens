9c40e4acece14deb6e8fcc1795efb0cfddce1220
marwan-at-work <marwan.sameer@gmail.com>
2018-08-04 13:29:05 -0400

    wip

--- scripts/get_buffalo.sh
+++ scripts/get_buffalo.sh
@@ -2,12 +2,14 @@
 
 set -xeuo pipefail
 
-TAR_GZ="buffalo_0.12.3_linux_amd64.tar.gz"
-BUFFALO_URL="https://github.com/gobuffalo/buffalo/releases/download/v0.12.3/${TAR_GZ}"
-BUFFALO_TARGET_BIN="./bin/buffalo"
+go get github.com/gobuffalo/buffalo/buffalo@v0.12.3
 
-curl -L -o ${TMPDIR}${TAR_GZ} ${BUFFALO_URL}
-tar -xzf ${TMPDIR}${TAR_GZ} -C ${TMPDIR}
-mkdir -p $(dirname ${BUFFALO_TARGET_BIN})
-mv ${TMPDIR}/buffalo-no-sqlite ${BUFFALO_TARGET_BIN}
-chmod +x ${BUFFALO_TARGET_BIN}
+# TAR_GZ="buffalo_0.12.3_linux_amd64.tar.gz"
+# BUFFALO_URL="https://github.com/gobuffalo/buffalo/releases/download/v0.12.3/${TAR_GZ}"
+# BUFFALO_TARGET_BIN="./bin/buffalo"
+
+# curl -L -o ${TMPDIR}${TAR_GZ} ${BUFFALO_URL}
+# tar -xzf ${TMPDIR}${TAR_GZ} -C ${TMPDIR}
+# mkdir -p $(dirname ${BUFFALO_TARGET_BIN})
+# mv ${TMPDIR}/buffalo-no-sqlite ${BUFFALO_TARGET_BIN}
+# chmod +x ${BUFFALO_TARGET_BIN}
