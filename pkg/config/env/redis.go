9bd4b63144c7c329e960b055b46aeefde206b2c8
Kun Devanny <kuniform@hotmail.com>
2018-08-05 02:01:09 +0100

    Revert "Add redis in-mem mock config for proxy"
    This reverts commit 3373947fb5ffa2d45ffc15144751f4a130d4f1c1.

--- pkg/config/env/redis.go
+++ pkg/config/env/redis.go
@@ -1,25 +1,11 @@
 package env
 
-import (
-	"strconv"
-
-	"github.com/gobuffalo/envy"
-)
+import "github.com/gobuffalo/envy"
 
 // RedisQueuePortWithDefault returns Redis queue port used by workers defined by ATHENS_REDIS_QUEUE_PORT.
 // Standard port is 6379
 func RedisQueuePortWithDefault(value string) string {
 	return envy.Get("ATHENS_REDIS_QUEUE_PORT", value)
-}
-
-// RedisMockInMem determines whether an in-memory worker is used to mock Redis for Proxy (Athens)
-func RedisMockInMem() bool {
-	boolStr := envy.Get("ATHENS_REDIS_MOCK_IN_MEM", "false")
-	enable, err := strconv.ParseBool(boolStr)
-	if err != nil {
-		return false
-	}
-	return enable
 }
 
 // OlympusRedisQueuePortWithDefault returns Redis queue port used by workers defined by OLYMPUS_REDIS_QUEUE_PORT.
