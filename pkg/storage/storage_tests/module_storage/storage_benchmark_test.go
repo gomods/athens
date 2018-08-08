554df258e1c95c3de33dc0c0a64f9bc4b883b574
Dinesh Kumar <dinesh.kumar@go-jek.com>
2018-08-08 18:24:58 +0530

    Fixing delete operation in delete benchmark

--- pkg/storage/storage_tests/module_storage/storage_benchmark_test.go
+++ pkg/storage/storage_tests/module_storage/storage_benchmark_test.go
@@ -64,14 +64,13 @@ func BenchmarkStorageDelete(b *testing.B) {
 		backend := store.Storage()
 
 		for i := 0; i < b.N; i++ {
-			err := backend.Save(context.Background(), fmt.Sprintf("%s-%d", module, i), version, mod, bytes.NewReader(zip), info)
-			require.NoError(b, err, "delete for storage %s failed", backend)
+			err := backend.Save(context.Background(), fmt.Sprintf("del-%s-%d", module, i), version, mod, bytes.NewReader(zip), info)
+			require.NoError(b, err, "save for storage %s module: %s failed", backend, i)
 		}
 
 		b.Run(fmt.Sprintf("delete module backend %s", store.StorageHumanReadableName()), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				err := backend.Save(context.Background(), fmt.Sprintf("%s-%d", module, i), version, mod, bytes.NewReader(zip), info)
-				require.NoError(b, err)
+				backend.Delete(context.Background(), fmt.Sprintf("del-%s-%d", module, i), version)
 			}
 		})
 
