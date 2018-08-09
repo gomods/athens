0ea0569299fd6fc8c83c9ba301c332ba26fddfa2
Aaron Schlesinger <arschles@gmail.com>
2018-08-09 12:44:47 -0700

    starting to test the list endpoint

---
+++ cmd/proxy/actions/list_test.go
@@ -0,0 +1,11 @@
+package actions
+
+func (a *ActionSuite) TestList() {
+	r := a.Require()
+
+	req := a.JSON("/github.com/gomods/athens/@v/list")
+	res := req.Get()
+	r.Equal(200, res.Code)
+	res.Body
+	res.Body
+}
