6c8a68765b3c77bcef2dc684013e1fcae3de9d64
Federico Paolinelli <fedepaol@gmail.com>
2018-08-14 00:24:47 +0200

    Removed commented early version of the test

--- cmd/proxy/actions/actions_test.go
+++ cmd/proxy/actions/actions_test.go
@@ -18,24 +18,3 @@ func Test_ActionSuite(t *testing.T) {
 	as := &ActionSuite{suite.NewAction(app)}
 	suite.Run(t, as)
 }
-
-/*
-func (a *ActionSuite) Test_Filter() {
-	r := a.Require()
-
-	// Public, expects to be redirected to olympus
-	req := a.JSON("/github.com/gomods/athens/@v/list")
-	res := req.Get()
-	r.Equal(303, res.Code)
-	r.Equal(GetOlympusEndpoint()+"/github.com/gomods/athens/@v/list", res.HeaderMap.Get("Location"))
-
-	// Excluded, expects a 403
-	req = a.JSON("/github.com/athens-artifacts/no-tags/@v/list")
-	res = req.Get()
-	r.Equal(403, res.Code)
-
-	// Private, the proxy is working and returns a 200
-	req = a.JSON("/github.com/athens-artifacts/happy-path/@v/list")
-	res = req.Get()
-	r.Equal(200, res.Code)
-}*/
