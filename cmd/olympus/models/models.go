1777208f05d19900f982246af697b95b10c6a9c8
Manu Gupta <manugupt1@gmail.com>
2018-08-17 19:07:48 -0400

    Completely remove pop

--- cmd/olympus/models/models.go
+++ cmd/olympus/models/models.go
@@ -1,15 +1,5 @@
 package models
 
-// DB is a connection to your database to be used
-// throughout your application.
-// var DB *pop.Connection
-
 func init() {
-	// var err error
-	// env := env.GoEnvironmentWithDefault("development")
-	// DB, err = pop.Connect(env)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// pop.Debug = env == "development"
+
 }
