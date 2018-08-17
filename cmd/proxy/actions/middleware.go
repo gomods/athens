66fd6500708c1f4323d41493c1ada7fa0df09ac9
Federico Paolinelli <fedepaol@gmail.com>
2018-08-17 22:56:28 +0200

    Fixed wrong logrus import

--- cmd/proxy/actions/middleware.go
+++ cmd/proxy/actions/middleware.go
@@ -7,7 +7,6 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/bketelsen/buffet"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gomods/athens/pkg/config/env"
@@ -15,6 +14,7 @@ import (
 	"github.com/gomods/athens/pkg/log"
 	"github.com/gomods/athens/pkg/module"
 	"github.com/gomods/athens/pkg/paths"
+	"github.com/sirupsen/logrus"
 )
 
 // MiddlewareFunc is a function that takes all that it needs to return
