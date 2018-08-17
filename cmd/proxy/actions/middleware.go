38e2af43ffb0859893e557722ca0e32b170974cc
Federico Paolinelli <fedepaol@gmail.com>
2018-08-17 22:09:13 +0200

    Renamed filter to middleware, all the middleware stuff in a single file

--- cmd/proxy/actions/middleware.go
+++ cmd/proxy/actions/middleware.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"strings"
@@ -8,14 +10,19 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/bketelsen/buffet"
 	"github.com/gobuffalo/buffalo"
+	"github.com/gomods/athens/pkg/config/env"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/log"
 	"github.com/gomods/athens/pkg/module"
 	"github.com/gomods/athens/pkg/paths"
 )
 
+// MiddlewareFunc is a function that takes all that it needs to return
+// a ready-to-go buffalo middleware.
 type MiddlewareFunc func(mf *module.Filter, entry log.Entry) buffalo.MiddlewareFunc
 
+// LogEntryMiddleware builds a log.Entry applying the request parameter to the given
+// log.Logger and propagates it to the given MiddlewareFunc
 func LogEntryMiddleware(middleware MiddlewareFunc, lggr *log.Logger, mf *module.Filter) buffalo.MiddlewareFunc {
 	return func(next buffalo.Handler) buffalo.Handler {
 		return func(c buffalo.Context) error {
@@ -71,10 +78,74 @@ func newFilterMiddleware(mf *module.Filter, entry log.Entry) buffalo.MiddlewareF
 	}
 }
 
+func newValidationMiddleware(mf *module.Filter, entry log.Entry) buffalo.MiddlewareFunc {
+	const op errors.Op = "actions.ValidationMiddleware"
+
+	return func(next buffalo.Handler) buffalo.Handler {
+		return func(c buffalo.Context) error {
+			sp := buffet.SpanFromContext(c).SetOperationName("validationMiddleware")
+			defer sp.Finish()
+
+			mod, err := paths.GetModule(c)
+
+			if err != nil {
+				// if there is no module the path we are hitting is not one related to modules, like /
+				return next(c)
+			}
+
+			// not checking the error. Not all requests include a version
+			// i.e. list requests path is like /{module:.+}/@v/list with no version parameter
+			version, _ := paths.GetVersion(c)
+
+			if validatorHook, ok := env.ValidatorHook(); ok && version != "" {
+				valid, err := validate(validatorHook, mod, version)
+				if err != nil {
+					entry.SystemErr(err)
+					return c.Render(http.StatusInternalServerError, nil)
+				}
+
+				if !valid {
+					return c.Render(http.StatusForbidden, nil)
+				}
+			}
+			return next(c)
+		}
+	}
+}
+
 func isPseudoVersion(version string) bool {
 	return strings.HasPrefix(version, "v0.0.0-")
 }
 
 func redirectToOlympusURL(u *url.URL) string {
 	return strings.TrimSuffix(GetOlympusEndpoint(), "/") + u.Path
+}
+
+type validationParams struct {
+	Module  string
+	Version string
+}
+
+func validate(hook, mod, ver string) (bool, error) {
+	const op errors.Op = "actions.validate"
+
+	toVal := &validationParams{mod, ver}
+	jsonVal, err := json.Marshal(toVal)
+	if err != nil {
+		return false, errors.E(op, err)
+	}
+
+	resp, err := http.Post(hook, "application/json", bytes.NewBuffer(jsonVal))
+	if err != nil {
+		return false, errors.E(op, err)
+	}
+
+	switch {
+	case resp.StatusCode == http.StatusOK:
+		return true, nil
+	case resp.StatusCode == http.StatusForbidden:
+		return false, nil
+	default:
+		return false, errors.E(op, "Unexpected status code ", resp.StatusCode)
+	}
 }
