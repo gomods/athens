39fc87402d02b250eed7da408b3110db04a9d49d
marwan-at-work <marwan.sameer@gmail.com>
2018-08-17 00:46:56 -0400

    Proxy: do not redirect on filter.Include

--- cmd/proxy/actions/filter.go
+++ cmd/proxy/actions/filter.go
@@ -42,8 +42,7 @@ func newFilterMiddleware(mf *module.Filter) buffalo.MiddlewareFunc {
 				return next(c)
 			case module.Include:
 				// TODO : spin up cache filling worker and serve the request using the cache
-				newURL := redirectToOlympusURL(c.Request().URL)
-				return c.Redirect(http.StatusSeeOther, newURL)
+				return next(c)
 			}
 
 			return next(c)
