92989de8537a6d24d3b4f486534bd11f3217bd2a
robjloranger <robjloranger@protonmail.com>
2018-08-26 12:47:00 -0700

    better output on failure
    also removes tilde from initial regex, git does not use tilde in diffs
    adds output of filenames with detected regex on failure

--- scripts/check_conflicts.sh
+++ scripts/check_conflicts.sh
@@ -11,8 +11,12 @@
 # CI build to fail. Merge conflict artifacts must be removed before continuing.
 
 git remote set-branches --add origin master && git fetch
-COUNT=$(git diff origin/master -- . ':!*.go' ':!go.mod' ':!go.sum' | grep -Ec "^[\+~][<>=]{7}\w{0,}")
+COUNT=$(git diff origin/master -- . ':!*.go' ':!go.mod' ':!go.sum' | grep -Ec "^\+[<>=]{7}\w{0,}")
 
 if (($COUNT > 0));then
+  echo "************************************************************"
+  echo "The following files contained merge conflict artifacts:\n"
+  exec git diff --name-only -G'^[<>=]{7}\w?' origin/master -- . ':!*.go' ':!go.mod' ':!go.sum'
+  echo "************************************************************"
   exit 1
 fi
