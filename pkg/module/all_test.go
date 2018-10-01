185000ea403cb00ea30a2acebcc9aae31735a774
marpio <marcinkowski@mailbox.org>
2018-10-01 13:50:34 +0200

    add comment

--- pkg/module/all_test.go
+++ pkg/module/all_test.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	// these values need to point to a real repository that has a tag
+	// github.com/NYTimes/gizmo is a example of a path that needs to be encoded so we can cover that case as well
 	repoURI = "github.com/NYTimes/gizmo"
 	version = "v0.1.4"
 )
