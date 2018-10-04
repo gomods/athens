97d8013876612616c40b1d7c6b15abd29aa03bd7
Marwan Sulaiman <marwan.sameer@gmail.com>
2018-10-04 19:16:24 -0400

    Refactor storage tests (#727)
    * Refactor storage tests
    
    * minio: check object err
    
    * remove inaccurate test
    
    * storage/fs: use memory

---
+++ pkg/storage/minio/minio_test.go
@@ -0,0 +1,53 @@
+package minio
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gomods/athens/pkg/config"
+	"github.com/gomods/athens/pkg/storage/compliance"
+)
+
+func TestBackend(t *testing.T) {
+	backend := getStorage(t)
+	compliance.RunTests(t, backend, backend.clear)
+}
+
+func BenchmarkBackend(b *testing.B) {
+	backend := getStorage(b)
+	compliance.RunBenchmarks(b, backend, backend.clear)
+}
+
+func (s *storageImpl) clear() error {
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+	objectCh := s.minioClient.ListObjectsV2(s.bucketName, "", true, doneCh)
+	for object := range objectCh {
+		if object.Err != nil {
+			return object.Err
+		}
+		if err := s.minioClient.RemoveObject(s.bucketName, object.Key); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getStorage(t testing.TB) *storageImpl {
+	url := os.Getenv("ATHENS_MINIO_ENDPOINT")
+	if url == "" {
+		t.SkipNow()
+	}
+
+	backend, err := NewStorage(&config.MinioConfig{
+		Endpoint: url,
+		Key:      "minio",
+		Secret:   "minio123",
+		Bucket:   "gomods",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return backend.(*storageImpl)
+}
--- pkg/storage/test_suite.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-// TestSuite is common interface which each storage needs to implement
-type TestSuite interface {
-	Storage() Backend
-	StorageHumanReadableName() string
-	Cleanup() error
-}
