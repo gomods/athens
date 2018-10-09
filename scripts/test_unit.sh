d33db0ed5f29efa133052ea02129f9be6fa6302d
t-tomalak <tomasztomalak@o2.pl>
2018-10-10 00:33:20 +0200

    Add jaeger tracer to helm charts

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -6,6 +6,8 @@ if [ -z ${GO_ENV} ]; then
     export GO_ENV="test"
 fi
 
+export GO111MODULE=on
+
 # Run the unit tests with the race detector and code coverage enabled
 set -xeuo pipefail
 go test -mod=vendor -race -coverprofile cover.out -covermode atomic ./...
