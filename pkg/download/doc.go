e3918ee509173bbd70cac5474ae039fb68f575d7
Henry Jenkins <henry@henryjenkins.name>
2018-10-12 11:21:16 +0100

    Remove reference to Zeus
    Use name Proxy for now.

--- pkg/download/doc.go
+++ pkg/download/doc.go
@@ -1,6 +1,4 @@
-// Package download provides buffalo handlers
-// that implement vgo's Download Protocol.
-// This is so that both Zeus and Olympus
-// can share the same download protocol
-// implementation.
+// Package download provides buffalo handlers that implement vgo's Download
+// Protocol. This is so that both Proxy and Olympus can share the same download
+// protocol implementation.
 package download
