6868a285e93012c35733187c5fbcf7f90fdfd83a
Henry Jenkins <henry@henryjenkins.name>
2018-10-17 14:34:05 +0100

    Update docs for Olympus removal

--- pkg/download/doc.go
+++ pkg/download/doc.go
@@ -1,4 +1,3 @@
 // Package download provides buffalo handlers that implement vgo's Download
-// Protocol. This is so that both Proxy and Olympus can share the same download
-// protocol implementation.
+// Protocol.
 package download
