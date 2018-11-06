0e54ca73e6eb4012711dc96cf75737ed1a371059
robjloranger <robjloranger@protonmail.com>
2018-11-05 17:24:31 -0800

    Fix some things
    remove commit SHA as build script will now create a hybrid for version
    as in the push-docker-images.sh script, code shamelessly stolen.
    
    change JSON function to Data and return a struct, the caller can do what
    they like with that data, i.e. marshall JSON for a response body.

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -1,25 +1,23 @@
 // Package build provides details of the built binary
 // The details are set using ldflags.
 //
-// The ldflags can be set either manually:
-// `go build -ldflags "-X github.com/gomods/athens/pkg/build.commitSHA=$(git rev-list -1 HEAD) -X github.com/gomods/athens/pkg/build.version=$(git describe --tags) -X github.com/gomods/athens/pkg/build.buildDate=$(date -u +%Y-%m-%d-%H:%M:%S-%Z)"`
+// The ldflags can be set manually for testing locally:
+// `go build -ldflags "-X github.com/gomods/athens/pkg/build.version=$(git describe --tags) -X github.com/gomods/athens/pkg/build.buildDate=$(date -u +%Y-%m-%d-%H:%M:%S-%Z)"`
 //
 // or using the build script in ./scripts.
 package build
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-// details represents known data for a given build
-type details struct {
+// Details represents known data for a given build
+type Details struct {
 	Version string
-	Commit  string
 	Date    string
 }
 
-var commitSHA, version, buildDate string
+var version, buildDate string
 
 // String returns build details as a string with formatting
 // suitable for console output.
@@ -27,19 +25,15 @@ var commitSHA, version, buildDate string
 // i.e.
 // Build Details:
 //         Version:        v0.1.0-155-g1a20f8b
-//         Commit SHA:     1a20f8b6a36136183f8533ae850a582716bbd577
 //         Date:           2018-11-05-14:33:14-UTC
 func String() string {
-	return fmt.Sprintf("Build Details:\n\tVersion:\t%s\n\tCommit SHA:\t%s\n\tDate:\t\t%s", version, commitSHA, buildDate)
+	return fmt.Sprintf("Build Details:\n\tVersion:\t%s\n\tDate:\t\t%s", version, buildDate)
 }
 
-// JSON returns build details JSON object as a slice of byte
-// for use in server response bodies
-func JSON() ([]byte, error) {
-	out := details{
+// Data returns build details as a struct
+func Data() Details {
+	return Details{
 		Version: version,
-		Commit:  commitSHA,
 		Date:    buildDate,
 	}
-	return json.Marshal(out)
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,22 @@
 # build.sh runs a few commands to generate build time details
 # which are stored in variables in ./pkg/build
 
-commit=$(git rev-list -1 HEAD)
-version=$(git describe --tags)
-date=$(date -u +%Y-%m-%d-%H:%M:%S-%Z)
+# Use the travis variables when available because travis clones different than what is on a local dev machine
+# VERSION = the tag if present, otherwise the short commit hash
+# BRANCH = the current branch, empty if not on a branch
+if [[ "${TRAVIS-}" == "true" ]]; then
+    VERSION=${TRAVIS_TAG:-${TRAVIS_COMMIT::7}}
+else
+    TAG=$(git describe --tags --exact-match 2> /dev/null || true)
+    COMMIT=$(git rev-parse --short=7 HEAD)
+    VERSION=${VERSION:-${TAG:-${COMMIT}}}
+fi
+
+DATE=$(date -u +%Y-%m-%d-%H:%M:%S-%Z)
 
 importPath="github.com/gomods/athens/pkg/build"
 
 export GO111MODULE=on
 export CGO_ENABLED=0 
 
-exec go build -mod=vendor -ldflags "-X $importPath.commitSHA=$commit -X $importPath.version=$version -X $importPath.buildDate=$date" -o /bin/athens-proxy ./cmd/proxy
+exec go build -mod=vendor -ldflags "-X $importPath.version=$VERSION -X $importPath.buildDate=$DATE" -o /bin/athens-proxy ./cmd/proxy
