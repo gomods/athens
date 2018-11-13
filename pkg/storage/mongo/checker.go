59b989d0646d39d6fd1066e05abe61c3cac7047d
Cixtor <cixtords@gmail.com>
2018-11-13 09:00:00 -0800

    Remove inefficient assignment of variable cxt in pkg/storage/mongo

--- pkg/storage/mongo/checker.go
+++ pkg/storage/mongo/checker.go
@@ -11,7 +11,7 @@ import (
 // Exists checks for a specific version of a module
 func (s *ModuleStore) Exists(ctx context.Context, module, vsn string) (bool, error) {
 	var op errors.Op = "mongo.Exists"
-	ctx, span := observ.StartSpan(ctx, op.String())
+	_, span := observ.StartSpan(ctx, op.String())
 	defer span.End()
 	c := s.s.DB(s.d).C(s.c)
 	count, err := c.Find(bson.M{"module": module, "version": vsn}).Count()
