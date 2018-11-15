af0fc6294d1a03bb39a721f352bc2fe7433c482a
Cixtor <cixtords@gmail.com>
2018-11-15 10:08:00 -0800

    Modify unit-test error reporting for the config package

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -327,20 +327,20 @@ func restoreEnv(envVars map[string]string) {
 func Test_checkFilePerms(t *testing.T) {
 	f1, err := ioutil.TempFile(os.TempDir(), "prefix-")
 	if err != nil {
-		t.FailNow()
+		t.Errorf("Cannot create 1st temp file: %s", err)
 	}
 	defer os.Remove(f1.Name())
 	if err = os.Chmod(f1.Name(), 0777); err != nil {
-		t.FailNow()
+		t.Errorf("Cannot chmod 1st temp file: %s", err)
 	}
 
 	f2, err := ioutil.TempFile(os.TempDir(), "prefix-")
 	if err != nil {
-		t.FailNow()
+		t.Errorf("Cannot create 2nd temp file: %s", err)
 	}
 	defer os.Remove(f2.Name())
 	if err = os.Chmod(f2.Name(), 0600); err != nil {
-		t.FailNow()
+		t.Errorf("Cannot chmod 2nd temp file: %s", err)
 	}
 
 	type args struct {
