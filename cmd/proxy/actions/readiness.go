1f25bdfb3f0354264dd249ce896c04118cc14f64
Audrius Karabanovas <auk@trustpilot.com>
2018-11-22 14:04:54 +0200

    Removing DNS check as that will be covered by storage.List() call

--- cmd/proxy/actions/readiness.go
+++ cmd/proxy/actions/readiness.go
@@ -3,16 +3,11 @@ package actions
 import (
 	"github.com/gobuffalo/buffalo"
 	"github.com/gomods/athens/pkg/storage"
-	"net"
 )
 
 func getReadinessHandler(s storage.Backend) buffalo.Handler {
 	return func(c buffalo.Context) error {
 		if _, err := s.List(c, "github.com/gomods/athens"); err != nil {
-			return c.Render(500, nil)
-		}
-
-		if _, err := net.LookupIP("github.com"); err != nil {
 			return c.Render(500, nil)
 		}
 
