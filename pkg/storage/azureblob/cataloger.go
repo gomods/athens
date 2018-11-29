8a3cb54124c059ce462c9db40be8977a3201e734
Federico Paolinelli <fedepaol@gmail.com>
2018-11-29 21:11:17 +0100

    Added cataloger method to azure storage

---
+++ pkg/storage/azureblob/cataloger.go
@@ -0,0 +1,16 @@
+package azureblob
+
+import (
+	"context"
+
+	"github.com/gomods/athens/pkg/paths"
+
+	"github.com/gomods/athens/pkg/errors"
+)
+
+// Catalog implements the (./pkg/storage).Cataloger interface
+// It returns a list of modules and versions contained in the storage
+func (s *Storage) Catalog(ctx context.Context, token string, elements int) ([]paths.AllPathParams, string, error) {
+	const op errors.Op = "azure.Catalog"
+	return nil, "", errors.E(op, errors.KindMethodNotImplemented)
+}
