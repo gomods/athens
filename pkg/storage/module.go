76d0651eecc7f177bfa1d1704c9314116347e7e3
Manu Gupta <manugupt1@gmail.com>
2018-12-09 10:13:53 -0500

    Add catalog endpoint for mongo

--- pkg/storage/module.go
+++ pkg/storage/module.go
@@ -1,10 +1,15 @@
 package storage
 
+import (
+	"github.com/globalsign/mgo/bson"
+)
+
 // Module represents a vgo module saved in a storage backend.
 type Module struct {
-	Module  string `bson:"module"`
-	Version string `bson:"version"`
-	Mod     []byte `bson:"mod"`
-	Zip     []byte `bson:"zip"`
-	Info    []byte `bson:"info"`
+	ID      bson.ObjectId `bson:"_id"`
+	Module  string        `bson:"module"`
+	Version string        `bson:"version"`
+	Mod     []byte        `bson:"mod"`
+	Zip     []byte        `bson:"zip"`
+	Info    []byte        `bson:"info"`
 }
