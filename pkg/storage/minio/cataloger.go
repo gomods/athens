1c286e53c5568505a2fc4c3038cf069def37a58b
Boris <zyqsempai@mail.ru>
2019-01-04 16:15:52 +0200

    Catalog fix

--- pkg/storage/minio/cataloger.go
+++ pkg/storage/minio/cataloger.go
@@ -6,10 +6,9 @@ import (
 	"strings"
 
 	"github.com/gomods/athens/pkg/observ"
-	"github.com/gomods/athens/pkg/config"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/paths"
-	minio "github.com/minio/minio-go"
+	"github.com/minio/minio-go"
 )
 
 // Catalog implements the (./pkg/storage).Cataloger interface
@@ -20,19 +19,15 @@ func (s *storageImpl) Catalog(ctx context.Context, token string, pageSize int) (
 	defer span.End()
 	queryToken := token
 	res := make([]paths.AllPathParams, 0)
-	count := pageSize
-	for count > 0 {
-		doneCh := make(chan struct{})
-		defer close(doneCh)
-		searchPrefix := queryToken + "/"
-		loo := s.minioClient.ListObjectsV2(s.bucketName, searchPrefix, true, doneCh)
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+	loo := s.minioClient.ListObjectsV2(s.bucketName, token, true, doneCh)
 
-		m, lastKey := fetchModsAndVersions(loo, count)
+	m, lastKey := fetchModsAndVersions(loo, pageSize)
 
-		res = append(res, m...)
-		count -= len(m)
-		queryToken = lastKey
-	}
+	res = append(res, m...)
+	queryToken = lastKey
+
 	return res, queryToken, nil
 }
 
@@ -41,9 +36,11 @@ func fetchModsAndVersions(objects <-chan minio.ObjectInfo, elementsNum int) ([]p
 	lastKey := ""
 
 	for o := range objects {
+		fmt.Println(o.Key)
 		if !strings.HasSuffix(o.Key, ".info") {
 			continue
 		}
+
 		p, err := parseMinioKey(&o)
 		if err != nil {
 			continue
@@ -62,8 +59,12 @@ func fetchModsAndVersions(objects <-chan minio.ObjectInfo, elementsNum int) ([]p
 
 func parseMinioKey(o *minio.ObjectInfo) (paths.AllPathParams, error) {
 	const op errors.Op = "minio.parseMinioKey"
-	m, v := config.ModuleVersionFromPath(o.Key)
+	parts := strings.Split(o.Key, "/")
+	fmt.Println(parts)
+	v := parts[len(parts)-1]
+	fmt.Println(v)
 
+	m := strings.Replace(o.Key, v + "/.info", "", -1)
 	if m == "" || v == "" {
 		return paths.AllPathParams{}, errors.E(op, fmt.Errorf("invalid object key format %s", o.Key))
 	}
