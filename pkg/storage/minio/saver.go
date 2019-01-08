9b70d557e10a07c7d9d56de2ef6b5665b54fada7
Manu Gupta <manugupt1@gmail.com>
2019-01-07 22:48:05 -0800

    Start generalizing upload code for minio

--- pkg/storage/minio/saver.go
+++ pkg/storage/minio/saver.go
@@ -7,28 +7,24 @@ import (
 
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/observ"
+	moduploader "github.com/gomods/athens/pkg/storage/module"
 	minio "github.com/minio/minio-go"
 )
 
-func (s *storageImpl) Save(ctx context.Context, module, vsn string, mod []byte, zip io.Reader, info []byte) error {
-	const op errors.Op = "storage.minio.Save"
+func (s *storageImpl) Save(ctx context.Context, module, version string, mod []byte, zip io.Reader, info []byte) error {
+	const op errors.Op = "minio.Save"
 	ctx, span := observ.StartSpan(ctx, op.String())
 	defer span.End()
-	dir := s.versionLocation(module, vsn)
-	modFileName := dir + "/" + "go.mod"
-	zipFileName := dir + "/" + "source.zip"
-	infoFileName := dir + "/" + vsn + ".info"
-	_, err := s.minioClient.PutObject(s.bucketName, modFileName, bytes.NewReader(mod), int64(len(mod)), minio.PutObjectOptions{})
+
+	err := moduploader.Upload(ctx, module, version, bytes.NewReader(info), bytes.NewReader(mod), zip, s.upload, 300)
 	if err != nil {
-		return errors.E(op, err)
-	}
-	_, err = s.minioClient.PutObject(s.bucketName, zipFileName, zip, -1, minio.PutObjectOptions{})
-	if err != nil {
-		return errors.E(op, err)
-	}
-	_, err = s.minioClient.PutObject(s.bucketName, infoFileName, bytes.NewReader(info), int64(len(info)), minio.PutObjectOptions{})
-	if err != nil {
-		return errors.E(op, err)
+		return errors.E(op, err, errors.M(module), errors.V(version))
 	}
 	return nil
+}
+
+func (s *storageImpl) upload(ctx context.Context, path, contentType string, stream io.Reader) error {
+	const op errors.Op = "minio.upload"
+	_, err := s.minioClient.PutObject(s.bucketName, path, stream, -1, minio.PutObjectOptions{ContentType: contentType})
+	return err
 }
