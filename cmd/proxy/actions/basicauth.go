4de9f4a4171133200c020e2994f7402b6185bd5a
marwan-at-work <marwan.sameer@gmail.com>
2019-01-11 12:55:02 -0500

    basicauth: log warning on healthz

--- cmd/proxy/actions/basicauth.go
+++ cmd/proxy/actions/basicauth.go
@@ -4,13 +4,24 @@ import (
 	"crypto/subtle"
 	"net/http"
 
+	"github.com/gomods/athens/pkg/log"
 	"github.com/gorilla/mux"
 )
+
+const healthWarning = "/healthz received none or incorrect Basic-Auth headers"
 
 func basicAuth(user, pass string) mux.MiddlewareFunc {
 	return func(h http.Handler) http.Handler {
 		f := func(w http.ResponseWriter, r *http.Request) {
 			if !checkAuth(r, user, pass) {
+				// Helpful hint for Kubernetes users:
+				// if they forget to send auth headers
+				// kubernetes silently fails, so a log
+				// might help them.
+				if r.URL.Path == "/healthz" {
+					lggr := log.EntryFromContext(r.Context())
+					lggr.Warnf(healthWarning)
+				}
 				w.Header().Set("WWW-Authenticate", `Basic realm="basic auth required"`)
 				w.WriteHeader(http.StatusUnauthorized)
 				return
