c36ef5dd3d94f30880458f8358c6a894de1d2397
Manu Gupta <manugupt1@gmail.com>
2019-01-14 23:45:53 -0800

    gc tryout

--- pkg/storage/module/upload.go
+++ pkg/storage/module/upload.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"runtime"
 	"time"
 
 	"github.com/gomods/athens/pkg/config"
@@ -43,9 +44,11 @@ func Upload(ctx context.Context, module, version string, info, mod, zip io.Reade
 			errChan <- fmt.Errorf("uploading %s.%s.%s failed: %s", module, version, ext, tctx.Err())
 		}
 	}
+
+	runtime.GC()
 	go saveOrAbort("info", "application/json", info)
-	saveOrAbort("mod", "text/plain", mod)
-	saveOrAbort("zip", "application/octet-stream", zip)
+	go saveOrAbort("mod", "text/plain", mod)
+	go saveOrAbort("zip", "application/octet-stream", zip)
 
 	var errs error
 	for i := 0; i < numFiles; i++ {
