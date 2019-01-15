e563494b1e6a8ce6a92129be3ae2bd2732e3a6da
marwan-at-work <marwan.sameer@gmail.com>
2019-01-15 18:10:56 -0500

    actions: improve basic auth tests

--- cmd/proxy/actions/basicauth_test.go
+++ cmd/proxy/actions/basicauth_test.go
@@ -1,57 +1,80 @@
 package actions
 
 import (
+	"bytes"
+	"context"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/gomods/athens/pkg/log"
+	"github.com/sirupsen/logrus"
 )
 
-type checkAuthTest struct {
-	name     string
-	user     string
-	pass     string
-	httpUser string
-	httpPass string
-	want     bool
+var basicAuthTests = [...]struct {
+	name   string
+	user   string
+	pass   string
+	path   string
+	logs   string
+	status int
+}{
+	{
+		name:   "happy_path",
+		user:   "correctUser",
+		pass:   "correctPass",
+		path:   "/",
+		logs:   "",
+		status: 404,
+	},
+	{
+		name:   "incorrect_username",
+		user:   "wrongUser",
+		pass:   "correctPass",
+		path:   "/",
+		logs:   "",
+		status: 401,
+	},
+	{
+		name:   "incorrect_password",
+		user:   "correctUser",
+		pass:   "wrongPassword",
+		path:   "/",
+		logs:   "",
+		status: 401,
+	},
+	{
+		name:   "log_on_healthz",
+		user:   "wrongUser",
+		pass:   "wrongPassword",
+		path:   "/healthz",
+		logs:   healthWarning,
+		status: 401,
+	},
 }
 
-var checkAuthTests = [...]checkAuthTest{
-	{
-		name:     "success",
-		user:     "athens",
-		pass:     "1234",
-		httpUser: "athens",
-		httpPass: "1234",
-		want:     true,
-	},
-	{
-		name:     "username incorrect",
-		user:     "athens",
-		pass:     "1234",
-		httpUser: "not athens",
-		httpPass: "1234",
-		want:     false,
-	},
-	{
-		name:     "password incorrect",
-		user:     "athens",
-		pass:     "1234",
-		httpUser: "athens",
-		httpPass: "not 1234",
-		want:     false,
-	},
-}
-
-func TestCheckAuth(t *testing.T) {
-	for _, testCase := range checkAuthTests {
-		t.Run(testCase.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodGet, "/", nil)
-			r.SetBasicAuth(testCase.httpUser, testCase.httpPass)
-
-			got := checkAuth(r, testCase.user, testCase.pass)
-			require.Equal(t, got, testCase.want)
+func TestBasicAuth(t *testing.T) {
+	mwFunc := basicAuth("correctUser", "correctPass")
+	handler := mwFunc(http.NotFoundHandler())
+	for _, tc := range basicAuthTests {
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, tc.path, nil)
+			r.SetBasicAuth(tc.user, tc.pass)
+			lggr := log.New("none", logrus.DebugLevel)
+			buf := &bytes.Buffer{}
+			lggr.Out = buf
+			ctx := log.SetEntryInContext(context.Background(), lggr)
+			r = r.WithContext(ctx)
+			handler.ServeHTTP(w, r)
+			resp := w.Result()
+			if resp.StatusCode != tc.status {
+				t.Fatalf("expected http status to be %v but got %v", tc.status, resp.StatusCode)
+			}
+			if !strings.Contains(buf.String(), tc.logs) {
+				t.Fatalf("expected logs to include: %s but got: %s", tc.logs, buf.String())
+			}
 		})
 	}
 }
