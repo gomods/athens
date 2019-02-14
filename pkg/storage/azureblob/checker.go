bca36bb3df39118afa0ee0d2acd839c4d62f0ef1
Aaron Schlesinger <arschles@protonmail.com>
2019-02-14 11:23:10 -0800

    Adding exists checks for all module files
    Ref https://github.com/gomods/athens/issues/1032

--- pkg/storage/azureblob/checker.go
+++ pkg/storage/azureblob/checker.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gomods/athens/pkg/config"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/observ"
+	"golang.org/x/sync/errgroup"
 )
 
 // Exists implements the (./pkg/storage).Checker interface
@@ -15,6 +16,32 @@ func (s *Storage) Exists(ctx context.Context, module string, version string) (bo
 	ctx, span := observ.StartSpan(ctx, op.String())
 	defer span.End()
 
-	pkgName := config.PackageVersionedName(module, version, "mod")
-	return s.client.BlobExists(ctx, pkgName)
+	fileNames := []string{
+		config.PackageVersionedName(module, version, "mod"),  // .mod
+		config.PackageVersionedName(module, version, "info"), // .info
+		config.PackageVersionedName(module, version, "zip"),  // .zip
+	}
+	// truth values for the existence of each file
+	availabilities := []bool{false, false, false}
+
+	g, ctx := errgroup.WithContext(ctx)
+	for i, name := range fileNames {
+		g.Go(func() error {
+			found, err := s.client.BlobExists(ctx, name)
+			if err != nil {
+				return err
+			}
+			availabilities[i] = found
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return false, err
+	}
+
+	available := true
+	for _, avail := range availabilities {
+		available = available && avail
+	}
+	return available, nil
 }
