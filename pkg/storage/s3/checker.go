d7c52e457bc238f6eecb014806e50bf3f6cf2a2e
Aaron Schlesinger <arschles@protonmail.com>
2019-02-14 16:42:52 -0800

    using the generic checker in the s3 checker

--- pkg/storage/s3/checker.go
+++ pkg/storage/s3/checker.go
@@ -6,9 +6,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/gomods/athens/pkg/config"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/observ"
+	modupl "github.com/gomods/athens/pkg/storage/module"
 )
 
 const (
@@ -21,20 +21,19 @@ func (s *Storage) Exists(ctx context.Context, module, version string) (bool, err
 	const op errors.Op = "s3.Exists"
 	ctx, span := observ.StartSpan(ctx, op.String())
 	defer span.End()
+	return modupl.Exists(ctx, module, version, func(ctx context.Context, name string) (bool, error) {
+		hoParams := &s3.HeadObjectInput{
+			Bucket: aws.String(s.bucket),
+			Key:    aws.String(name),
+		}
+		if _, err := s.s3API.HeadObjectWithContext(ctx, hoParams); err != nil {
+			if err.(awserr.Error).Code() == s3ErrorCodeNotFound {
+				return false, nil
+			}
 
-	pkgName := config.PackageVersionedName(module, version, "mod")
-	hoParams := &s3.HeadObjectInput{
-		Bucket: aws.String(s.bucket),
-		Key:    aws.String(pkgName),
-	}
-
-	if _, err := s.s3API.HeadObjectWithContext(ctx, hoParams); err != nil {
-		if err.(awserr.Error).Code() == s3ErrorCodeNotFound {
-			return false, nil
+			return false, errors.E(op, err, errors.M(module))
 		}
 
-		return false, errors.E(op, err, errors.M(module))
-	}
-
-	return true, nil
+		return true, nil
+	})
 }
