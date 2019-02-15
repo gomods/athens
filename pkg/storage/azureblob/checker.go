6a52d88636675560e585bf4b38d1454099a758f5
Aaron Schlesinger <arschles@protonmail.com>
2019-02-14 16:42:12 -0800

    using the generic checker in the azure blob checker

--- pkg/storage/azureblob/checker.go
+++ pkg/storage/azureblob/checker.go
@@ -3,10 +3,9 @@ package azureblob
 import (
 	"context"
 
-	"github.com/gomods/athens/pkg/config"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/observ"
-	"golang.org/x/sync/errgroup"
+	modupl "github.com/gomods/athens/pkg/storage/module"
 )
 
 // Exists implements the (./pkg/storage).Checker interface
@@ -16,32 +15,5 @@ func (s *Storage) Exists(ctx context.Context, module string, version string) (bo
 	ctx, span := observ.StartSpan(ctx, op.String())
 	defer span.End()
 
-	fileNames := []string{
-		config.PackageVersionedName(module, version, "mod"),  // .mod
-		config.PackageVersionedName(module, version, "info"), // .info
-		config.PackageVersionedName(module, version, "zip"),  // .zip
-	}
-	// truth values for the existence of each file
-	availabilities := []bool{false, false, false}
-
-	g, ctx := errgroup.WithContext(ctx)
-	for i, name := range fileNames {
-		g.Go(func() error {
-			found, err := s.client.BlobExists(ctx, name)
-			if err != nil {
-				return err
-			}
-			availabilities[i] = found
-			return nil
-		})
-	}
-	if err := g.Wait(); err != nil {
-		return false, err
-	}
-
-	available := true
-	for _, avail := range availabilities {
-		available = available && avail
-	}
-	return available, nil
+	return modupl.Exists(ctx, module, version, s.client.BlobExists)
 }
