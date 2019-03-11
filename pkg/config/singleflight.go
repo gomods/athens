fb9437ddffe218cb25b0ba247eecdc002ddb4a48
Marwan Sulaiman <marwan.sameer@gmail.com>
2019-03-11 02:48:04 -0400

    pkg/stash: implement a Redis lock for stashing (#1116)
    * pkg/stash: implement a Redis lock for stashing
    
    * fix tests
    
    * fix op

--- pkg/config/singleflight.go
+++ pkg/config/singleflight.go
@@ -4,7 +4,8 @@ package config
 // backend configurations for a distributed
 // lock or single flight mechanism.
 type SingleFlight struct {
-	Etcd *Etcd
+	Etcd  *Etcd
+	Redis *Redis
 }
 
 // Etcd holds client side configuration
@@ -12,4 +13,10 @@ type SingleFlight struct {
 // Etcd backends.
 type Etcd struct {
 	Endpoints string `envconfig:"ATHENS_ETCD_ENDPOINTS"`
+}
+
+// Redis holds the client side configuration
+// to connect to redis as a SingleFlight implementation.
+type Redis struct {
+	Endpoint string `envconfig:"ATHENS_REDIS_ENDPOINT"`
 }
