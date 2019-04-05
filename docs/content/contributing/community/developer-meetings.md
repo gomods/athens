93c7c58e18977e337c785334504549753a1a7403
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-05 12:21:09 -0700

    trying to see if time zones work
    this works locally but I have no idea why...

--- docs/content/contributing/community/developer-meetings.md
+++ docs/content/contributing/community/developer-meetings.md
@@ -16,8 +16,8 @@ We meet at this [zoom](https://zoom.us/) video Chat URL: [https://aka.ms/athensd
 
 We rotate the time of the meeting weekly, so that as many people as possible get a chance to attend the meeting at a time appropriate for their time zone. The rotation of times is below:
 
-- Thursday on odd week numbers of each month: 22:00 UTC ()
-- Thursday on even week numbers of each month: 20:00 UTC
+- Thursday on odd week numbers of each month: 22:00 UTC (<span id="time22"></span> locally)
+- Thursday on even week numbers of each month: 20:00 UTC (<span id="time20"></span> locally)
 
 Each developer meeting starts with a quick update on the project from the maintainers, and then we discuss things on the agenda.
 
@@ -35,3 +35,8 @@ The [agenda google doc](https://docs.google.com/document/d/1xpvgmR1Fq4iy1j975Tb4
 
 > * (Bob) Let’s talk about something cool!
 > * (Alice) Let’s talk about cool thing #2!
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.23/moment-timezone.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.4/jstz.min.js"></script>
+<script src="/js/dev-meetings.js"></script>
---
+++ docs/static/js/dev-meetings.js
@@ -0,0 +1,45 @@
+// get next thursday
+const nextThurs = moment().day(4 + 7);
+
+// get next thursday in UTC for 22:00 and 20:00
+const nextThurs22 = new Date(Date.UTC(
+    nextThurs.year(),
+    nextThurs.month(),
+    nextThurs.date(),
+    22
+));
+const nextThurs20 = new Date(Date.UTC(
+    nextThurs.year(),
+    nextThurs.month(),
+    nextThurs.date(),
+    20
+));
+
+
+// get next thursday in moment format for each time
+// (so that we can more easily print it out)
+const time20 = moment().
+    year(nextThurs20.getFullYear()).
+    month(nextThurs20.getMonth()).
+    date(nextThurs20.getDate()).
+    hour(nextThurs20.getHours()).
+    minute(nextThurs20.getMinutes());
+const time22 = moment().
+    year(nextThurs22.getFullYear()).
+    month(nextThurs22.getMonth()).
+    date(nextThurs22.getDate()).
+    hour(nextThurs22.getHours()).
+    minute(nextThurs22.getMinutes());
+
+
+const time20ID = document.getElementById("time20");
+time20ID.innerHTML = nextThurs20.getHours() + ":00";
+
+const time22ID = document.getElementById("time22");
+time22ID.innerHTML = nextThurs22.getHours() + ":00";
+// const tz = jstz.guess();
+// console.log("you are in timezone " + tz.name());
+// var utc20 = moment(now).hour(20);
+// var utc22 = moment().hour(22);
+// time20.innerHTML = utc20.add(6).format("hz");
+// time22.innerHTML = utc22.add(6).format("hz");
