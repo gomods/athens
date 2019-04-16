fa380716bd79d1e21dd3d139efe80de7c77569ea
Arpit Gogia <aarpitgogia@gmail.com>
2019-04-17 02:00:47 +0530

    gofmt related changes

--- pkg/storage/module.go
+++ pkg/storage/module.go
@@ -7,9 +7,9 @@ import (
 // Module represents a vgo module saved in a storage backend.
 type Module struct {
 	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	Module  string        `bson:"module"`
-	Version string        `bson:"version"`
-	Mod     []byte        `bson:"mod"`
-	Zip     []byte        `bson:"zip"`
-	Info    []byte        `bson:"info"`
+	Module  string             `bson:"module"`
+	Version string             `bson:"version"`
+	Mod     []byte             `bson:"mod"`
+	Zip     []byte             `bson:"zip"`
+	Info    []byte             `bson:"info"`
 }
