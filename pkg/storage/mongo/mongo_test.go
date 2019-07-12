e56635a5ba6f3448fc0fbeb6f0f470217d8919b0
Bob Maertz <r.maertz@gmail.com>
2019-07-12 08:13:26 -0400

    WIP: Added tests to query implemetation.

--- pkg/storage/mongo/mongo_test.go
+++ pkg/storage/mongo/mongo_test.go
@@ -1,12 +1,17 @@
 package mongo
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/gomods/athens/pkg/config"
+	"github.com/gomods/athens/pkg/errors"
+	"github.com/gomods/athens/pkg/storage"
 	"github.com/gomods/athens/pkg/storage/compliance"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -34,4 +39,36 @@ func getStorage(tb testing.TB) *ModuleStore {
 	require.NoError(tb, err)
 
 	return backend
+}
+
+func TestGetterQueryModuleNotFoundError(t *testing.T) {
+	modname, ver := "xxx", "yyy"
+
+	ctx := context.Background()
+	backend := getStorage(t)
+
+	_, err := query(ctx, backend, modname, ver)
+	require.Error(t, err)
+	require.Equal(t, errors.KindNotFound, errors.Kind(err))
+}
+
+func TestGetterQueryModuleExists(t *testing.T) {
+	modname, ver := "getTestModule", "v1.2.3"
+	mock := &storage.Version{
+		Info: []byte("123"),
+		Mod:  []byte("456"),
+		Zip:  ioutil.NopCloser(bytes.NewReader([]byte("789"))),
+	}
+
+	ctx := context.Background()
+	backend := getStorage(t)
+
+	zipBts, _ := ioutil.ReadAll(mock.Zip)
+	backend.Save(ctx, modname, ver, mock.Mod, bytes.NewReader(zipBts), mock.Info)
+	defer backend.Delete(ctx, modname, ver)
+
+	info, err := query(ctx, backend, modname, ver)
+	require.NoError(t, err)
+	require.Equal(t, mock.Info, info.Info)
+	require.Equal(t, mock.Mod, info.Mod)
 }
