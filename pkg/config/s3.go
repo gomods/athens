5c2e35bad4aa272f1bba4a653cac4e2777848d57
Manu Gupta <manugupt1@gmail.com>
2019-08-12 22:55:09 -0700

    gofmt

--- pkg/config/s3.go
+++ pkg/config/s3.go
@@ -2,12 +2,12 @@ package config
 
 // S3Config specifies the properties required to use S3 as the storage backend
 type S3Config struct {
-	Region                  string `validate:"required" envconfig:"AWS_REGION"`
-	Key                     string `envconfig:"AWS_ACCESS_KEY_ID"`
-	Secret                  string `envconfig:"AWS_SECRET_ACCESS_KEY"`
-	Token                   string `envconfig:"AWS_SESSION_TOKEN"`
-	Bucket                  string `validate:"required" envconfig:"ATHENS_S3_BUCKET_NAME"`
-	UseDefaultConfiguration bool   `envconfig:"AWS_USE_DEFAULT_CONFIGURATION"`
-	CredentialsEndpoint     string `envconfig:"AWS_CREDENTIALS_ENDPOINT"`
+	Region                             string `validate:"required" envconfig:"AWS_REGION"`
+	Key                                string `envconfig:"AWS_ACCESS_KEY_ID"`
+	Secret                             string `envconfig:"AWS_SECRET_ACCESS_KEY"`
+	Token                              string `envconfig:"AWS_SESSION_TOKEN"`
+	Bucket                             string `validate:"required" envconfig:"ATHENS_S3_BUCKET_NAME"`
+	UseDefaultConfiguration            bool   `envconfig:"AWS_USE_DEFAULT_CONFIGURATION"`
+	CredentialsEndpoint                string `envconfig:"AWS_CREDENTIALS_ENDPOINT"`
 	AwsContainerCredentialsRelativeURI string `envconfig:"AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"`
 }
