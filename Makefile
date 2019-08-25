df87f191fbfeca3ec2c881258444fb0a0fa2598e
Rick <1358735+rickb777@users.noreply.github.com>
2019-08-25 23:16:48 +0100

    First implementation; older systemd is not yet supported; SysV is not supported at all

--- Makefile
+++ Makefile
@@ -4,8 +4,12 @@ DATE=$(shell date -u +%Y-%m-%d-%H:%M:%S-%Z)
 build: ## build the athens proxy
 	cd cmd/proxy && go build
 
+.PHONY: build-ver
 build-ver: ## build the athens proxy with version number
 	GO111MODULE=on CGO_ENABLED=0 GOPROXY="https://proxy.golang.org" go build -ldflags "-X github.com/gomods/athens/pkg/build.version=$(VERSION) -X github.com/gomods/athens/pkg/build.buildDate=$(DATE)" -o athens ./cmd/proxy
+
+athens:
+	$(MAKE) build-ver
 
 # The build-image step creates a docker image that has all the tools required
 # to perform some CI build steps, instead of relying on them being installed locally
@@ -101,6 +105,10 @@ down:
 .PHONY: dev-teardown
 dev-teardown:
 	docker-compose -p athensdev down -v
+
+.PHONY: clean
+clean: ## delete all locally-built artefacts (not including docker images)
+	rm -f athens cmd/proxy/proxy
 
 .PHONY: help
 help: ## display help page
---
+++ scripts/systemd.sh
@@ -0,0 +1,173 @@
+#!/bin/bash -e
+# This script manages installation using SystemD, if available, or SysV as a fall-back.
+#
+# Usage:
+#  systemd.sh [ install | remove | status | log ]
+
+cd $(dirname $0)/..
+[[ -n $VERSION ]] || export VERSION=unset
+
+function ensureSystemdIsPresent
+{
+  if [[ $( uname -s ) != "Linux" ]]; then
+    echo "SystemD can only be used on Linux systems."
+    exit 1
+  fi
+
+  id=$(uname -msn)
+
+  if command -v systemctl >/dev/null; then
+    echo -n "$id has "
+    systemctl --version | head -1
+
+  elif [ -d /etc/init.d ]; then
+    echo "$id has SysV."
+
+  else
+    echo "Neither SystemD nor SysV is available in this operating system."
+    lsb_release -a
+    exit 1
+
+  fi
+}
+
+# doInstallConfig installs the config file
+function doInstallConfig
+{
+  if [ ! -r config.toml ]; then
+    echo "Missing: config.toml"
+    echo "Copy & modify config.dev.toml as needed."
+    exit 1
+  fi
+  sudo mkdir -p /etc/athens
+  sudo install -v -o root -g root -m 644 config.toml /etc/athens
+
+  # if storage is on disk, this is where the database goes (see scripts/service/athens.service)
+  ATHENS_DISK_STORAGE_ROOT=/var/run/athens
+  sudo mkdir -p $ATHENS_DISK_STORAGE_ROOT
+  sudo chown www-data $ATHENS_DISK_STORAGE_ROOT
+  sudo chgrp www-data $ATHENS_DISK_STORAGE_ROOT
+}
+
+# doInstallBinary copies the Athens binary to /usr/local/bin with the necessary settings.
+function doInstallBinary
+{
+  [ -f athens ] || make athens
+
+  if [ ! -x /usr/local/bin/athens -o athens -nt /usr/local/bin/athens ]; then
+
+    [ -f /etc/systemd/system/athens.service ] && sudo systemctl stop athens
+    [ -f /etc/init.d/athens ] && sudo /etc/init.d/athens stop
+
+    sudo install -v -o root -g root athens /usr/local/bin
+  fi
+
+  # Give the athens binary the ability to bind to privileged ports (e.g. 80, 443) as a non-root user:
+  command -v setcap >/dev/null && sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/athens
+}
+
+# doInstallSystemd sets up the SystemD service unit.
+function doInstallSystemd
+{
+  sudo install -v -o root -g root -m 644 scripts/service/athens.service /etc/systemd/system
+  sudo systemctl daemon-reload
+  sudo systemctl enable athens
+  sudo systemctl start athens
+}
+
+# doInstall builds and installs the binary as a SystemD unit
+function doInstall
+{
+  doInstallConfig
+  doInstallBinary
+  doInstallSystemd
+}
+
+# doRemove deletes the SystemD unit and cleans up log files etc
+function doRemove
+{
+  if [ -f /etc/systemd/system/athens.service ]; then
+    sudo systemctl stop athens
+    sudo rm -f /etc/systemd/system/athens.service
+    # Reset systemctl
+    sudo systemctl daemon-reload
+    echo "SystemD installation was removed."
+
+  elif [ -f /etc/init.d/athens ]; then
+    sudo service athens stop
+    sudo update-rc.d athens remove
+    sudo rm -f /etc/init.d/athens
+    echo "SysV installation was removed."
+
+  fi
+
+  sudo rm -rf /etc/athens /etc/ssl/athens /usr/local/bin/athens /var/log/athens.log /var/www/.athens
+}
+
+# doStatus shows what is installed, if anything, and whether it is running
+function doStatus
+{
+  if [ -x /usr/local/bin/athens ]; then
+    echo "Athens is /usr/local/bin/athens"
+    /usr/local/bin/athens --version
+  else
+    echo "Athens is absent (no /usr/local/bin/athens)."
+    exit 0
+  fi
+
+  if [ -f /etc/systemd/system/athens.service ]; then
+    echo
+    echo "SystemD: /etc/systemd/system/athens.service exists."
+    sudo systemctl status athens ||:
+
+  elif [ -f /etc/init.d/athens ]; then
+    echo
+    echo "SysV: /etc/init.d/athens exists."
+    sudo service athens status ||:
+
+  else
+    echo "Athens is not installed as a service."
+  fi
+}
+
+# showLog shows the relevant lines in syslog
+function showLog
+{
+  if [ -x /usr/local/bin/athens ]; then
+    echo "Athens is /usr/local/bin/athens"
+    /usr/local/bin/athens --version
+  else
+    echo "Athens is absent (no /usr/local/bin/athens)."
+    exit 0
+  fi
+
+  if [ -f /etc/systemd/system/athens.service ]; then
+    fgrep athens /var/log/syslog | fgrep "$(date '+%b %d')"
+
+  elif [ -f /etc/init.d/athens ]; then
+    fgrep athens /var/log/syslog | fgrep "$(date '+%b %d')"
+
+  else
+    echo "Athens is not installed as a service."
+  fi
+}
+
+### Main script ###doStatus
+
+case $1 in
+  install)
+    ensureSystemdIsPresent; doInstall ;;
+
+  remove|uninstall)
+    ensureSystemdIsPresent; dostatusRemove  ;;
+
+  status)
+    ensureSystemdIsPresent; doStatus  ;;
+
+  log)
+    ensureSystemdIsPresent; showLog  ;;
+
+  *)
+    echo "Usage: $0 [ install | remove | status | log"
+    exit 1 ;;
+esac
