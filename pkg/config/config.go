2051a333087b8eba4e61a87489109b1d1195cd6e
Kyle Hagan <kh448p@att.com>
2019-09-26 11:29:25 -0700

    Allow EnvList.Validate to have multiple = for GODEBUG var

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -85,7 +85,8 @@ func (el *EnvList) Add(key, value string) {
 func (el EnvList) Validate() error {
 	const op errors.Op = "EnvList.Validate"
 	for _, env := range el {
-		if strings.Count(env, "=") != 1 {
+		// excluding GODEBUG because it can have multiple "=", such as GODEBUG=netdns=cgo
+		if strings.Count(env, "=") != 1 && !strings.HasPrefix(env, "GODEBUG=") {
 			return errors.E(op, fmt.Errorf("incorrect env format: %v", env))
 		}
 	}
