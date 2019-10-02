ed66d85f3fa7d8cef17aad567c0c3e719e76eba1
Rob Prentiss <rob@prentiss.name>
2019-10-02 05:45:58 -0700

    Use glob matching for package paths (#1409)
    - Match the behavior of GOPRIVATE/GONOPROXY/GONOSUMDB

--- pkg/paths/path.go
+++ pkg/paths/path.go
@@ -2,6 +2,8 @@ package paths
 
 import (
 	"net/http"
+	"path"
+	"strings"
 
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gorilla/mux"
@@ -49,4 +51,29 @@ func GetAllParams(r *http.Request) (*AllPathParams, error) {
 	}
 
 	return &AllPathParams{Module: mod, Version: version}, nil
+}
+
+// MatchesPattern reports whether the path prefix of target matches
+// pattern (as defined by path.Match)
+//
+// This tries to keep the same behavior as GOPRIVATE/GONOPROXY/GONOSUMDB,
+// and is adopted from:
+// https://github.com/golang/go/blob/a11644a26557ea436d456f005f39f4e01902bafe/src/cmd/go/internal/str/path.go#L58
+func MatchesPattern(pattern, target string) bool {
+	n := strings.Count(pattern, "/")
+	prefix := target
+	for i := 0; i < len(target); i++ {
+		if target[i] == '/' {
+			if n == 0 {
+				prefix = target[:i]
+				break
+			}
+			n--
+		}
+	}
+	if n > 0 {
+		return false
+	}
+	matched, _ := path.Match(pattern, prefix)
+	return matched
 }
