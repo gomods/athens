555c9e389a64fa62422ed9a11e5c7d44057f63c3
Marwan Sulaiman <marwan.sameer@gmail.com>
2020-01-10 11:51:18 -0500

    always use request logging middleware

--- pkg/log/format.go
+++ pkg/log/format.go
@@ -1,6 +1,13 @@
 package log
 
 import (
+	"bytes"
+	"fmt"
+	sort "sort"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,7 +22,42 @@ func getGCPFormatter() logrus.Formatter {
 }
 
 func getDevFormatter() logrus.Formatter {
-	return &logrus.TextFormatter{}
+	return devFormatter{}
+}
+
+type devFormatter struct{}
+
+func (devFormatter) Format(e *logrus.Entry) ([]byte, error) {
+	var buf bytes.Buffer
+	sprint := color.CyanString
+	switch e.Level {
+	case logrus.DebugLevel:
+		sprint = color.New(0xffccc).Sprintf
+	case logrus.WarnLevel:
+		sprint = color.YellowString
+	case logrus.ErrorLevel:
+		sprint = color.RedString
+	}
+	lvl := strings.ToUpper(e.Level.String())
+	buf.WriteString(sprint(lvl))
+	buf.WriteString("[" + e.Time.Format(time.Kitchen) + "]")
+	buf.WriteString(": ")
+	buf.WriteString(e.Message)
+	buf.WriteByte('\t')
+	for _, k := range sortFields(e.Data) {
+		fmt.Fprintf(&buf, "%s=%s ", color.MagentaString(k), e.Data[k])
+	}
+	buf.WriteByte('\n')
+	return buf.Bytes(), nil
+}
+
+func sortFields(data logrus.Fields) []string {
+	keys := []string{}
+	for k := range data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
 
 func getDefaultFormatter() logrus.Formatter {
