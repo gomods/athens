af25de5b3b7b486e63846ac4510c115ce222e0f4
vapod <v.podoinikov@yandex.ru>
2020-02-03 17:25:54 +0300

    Add forcepathstyle config for s3

--- pkg/config/s3.go
+++ pkg/config/s3.go
@@ -8,6 +8,7 @@ type S3Config struct {
 	Token                              string `envconfig:"AWS_SESSION_TOKEN"`
 	Bucket                             string `validate:"required" envconfig:"ATHENS_S3_BUCKET_NAME"`
 	UseDefaultConfiguration            bool   `envconfig:"AWS_USE_DEFAULT_CONFIGURATION"`
+	ForcePathStyle                     bool   `evnconfig:"AWS_FORCE_PATH_STYLE"`
 	CredentialsEndpoint                string `envconfig:"AWS_CREDENTIALS_ENDPOINT"`
 	AwsContainerCredentialsRelativeURI string `envconfig:"AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"`
 	Endpoint                           string `evnconfig:"AWS_ENDPOINT"`
