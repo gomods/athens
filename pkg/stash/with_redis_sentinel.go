8791b2e9388db544c3ae642a8d028da4c43eee93
Ted Wexler <ted@stuckinacan.com>
2020-02-24 21:19:56 -0500

    Upgrade the redis client

--- pkg/stash/with_redis_sentinel.go
+++ pkg/stash/with_redis_sentinel.go
@@ -1,19 +1,31 @@
 package stash
 
 import (
-	"github.com/go-redis/redis"
+	errs "errors"
+
+	"github.com/go-redis/redis/v7"
 	"github.com/gomods/athens/pkg/errors"
 	"github.com/gomods/athens/pkg/storage"
+)
+
+var (
+	// ErrNoEndpoints is returned when no endpoints were provided
+	ErrNoEndpoints = errs.New("no endpoints provided")
 )
 
 // WithRedisSentinelLock returns a distributed singleflight
 // with a redis cluster that utilizes sentinel for quorum and failover
 func WithRedisSentinelLock(endpoints []string, master, password string, checker storage.Checker) (Wrapper, error) {
 	const op errors.Op = "stash.WithRedisSentinelLock"
+	// The redis client constructor does not return an error when no endpoints
+	// are provided, so we check for ourselves.
+	if len(endpoints) == 0 {
+		return nil, ErrNoEndpoints
+	}
 	client := redis.NewFailoverClient(&redis.FailoverOptions{
-		MasterName:    master,
-		SentinelAddrs: endpoints,
-		Password:      password,
+		MasterName:       master,
+		SentinelAddrs:    endpoints,
+		SentinelPassword: password,
 	})
 	_, err := client.Ping().Result()
 	if err != nil {
