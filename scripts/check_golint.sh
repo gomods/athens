4572813a50b9a6110fdc74547ad0f592e01b7619
Rob Lee <rob.lee@live.co.uk>
2021-12-09 16:11:56 +0000

    Set GO111MODULE to auto in golint script– #1743
    As mentioned in #1743 the Go modules environment flag is set to `off`
    in the script which appears to cause a warning message for each module
    of the codebase that it is "not in GOROOT".
    
    Set to `auto` as this allows the same build to be run the original way
    should someone choose to delete the `go.mod` file from the project root.

--- scripts/check_golint.sh
+++ scripts/check_golint.sh
@@ -4,4 +4,4 @@
 # Run the linter on everything except generated code
 set -euo pipefail
 
-golint -set_exit_status $(GO111MODULE=off go list ./... | grep -v '/mocks')
+golint -set_exit_status $(GO111MODULE=auto go list ./... | grep -v '/mocks')
