fc2ddc534063a466c0d9c05911b94c7dcd21f5ee
daviesmcarthur-del <daviesmcarthur@gmail.com>
2026-01-12 20:52:21 +0100

    Update backend.go

--- pkg/storage/backend.go
+++ pkg/storage/backend.go
@@ -7,3 +7,10 @@ type Backend interface {
 	Saver
 	Deleter
 }
+# Env override: ATHENS_STORAGE_TYPE
+StorageType = "external"
+
+[Storage]
+    [Storage.External]
+        # Env override: ATHENS_EXTERNAL_STORAGE_URL
+        URL = "http://localhost:9090"
